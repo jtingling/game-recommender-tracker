e1ed5fc8214ee2bd5e06ce0ed7a4347c3b9d19d2
Jacob T <jacob.tingling@protonmail.com>
2021-04-22 17:00:44 -0400

    Connected to backend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import { FormControl, InputLabel, Input, Button, Card, CardContent } from '@material-ui/core';
 
 function App() {
+  const [gameName, setGameName] = useState();
+  const [gameData, setGameData] = useState();
+  const [recommendations, setRecommendations] = useState();
+  const handleChange = (e) => {
+    setGameName(e.target.value)
+  }
+  const submitGame = (e) => {
+    console.log(gameName)
+    e.preventDefault();
+    fetch(`http://localhost:5000/games/${gameName}`)
+      .then(response => response.json())
+      .then(data => setGameData(data))
+  }
+  const submitRecommendations = () => {
+    fetch(`http://localhost:5000/recommendations`)
+      .then(response => response.json())
+      .then(data => setRecommendations(data))
+  }
+
+  const handleGameData = () => {
+    return (
+      <ul>{gameData.map((game) => {
+        return (
+          <Card>
+            <CardContent>
+              <li key={gameData.id}>{game.name}</li>
+              {console.log(game.name)}
+              <img src={`https://static-cdn.jtvnw.net/ttv-boxart/${game.name}-300x300.jpg`} alt='box-art' />
+            </CardContent>
+          </Card>
+        )
+      })}</ul>
+    )
+  }
+  const handleRecommendedData = () => {
+    return (
+      <ul>{recommendations.Similar.Results.map((games) => {
+        let name = games.Name.replaceAll(' ', "%20")
+        console.log(name);
+        return (
+          <Card>
+            <li>{games.Name}</li>
+            <img src={`https://static-cdn.jtvnw.net/ttv-boxart/${name}-300x300.jpg`} alt='box-art' />
+          </Card>
+        )
+      })}</ul>
+    )
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <form onSubmit={submitGame}>
+        <FormControl >
+          <InputLabel htmlFor="my-input">Game Name</InputLabel>
+          <Input id="my-input" aria-describedby="my-helper-text" value={gameName} onChange={handleChange} />
+        </FormControl>
+      </form>
+      <h1>Search Results:</h1>
+      {
+        gameData !== undefined ? handleGameData() : <></>
+      }
+      <h1>Recommendations:</h1>
+      <Button onClick={() => submitRecommendations()}>Get Recommendations</Button>
+      {
+        recommendations !== undefined ? handleRecommendedData() : <></>
+      }
+    </>
+  )
 }
 
 export default App;
