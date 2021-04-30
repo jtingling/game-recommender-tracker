01fa17628504c0fe42927362a8d1e0ed8d7bfe72
Jacob T <jacob.tingling@protonmail.com>
2021-04-30 13:24:38 -0400

    Added conditional rendering for GameDetail component and image fetching.

--- src/GameCard.js
+++ src/GameCard.js
@@ -1,3 +1,5 @@
+import { useContext, useState, useEffect } from 'react';
+import { GameContext } from './App'
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
@@ -9,6 +11,8 @@ import Typography from '@material-ui/core/Typography';
 import Chip from '@material-ui/core/Chip';
 import Box from '@material-ui/core/Box';
 import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
+
+import GameDetail from './GameDetail'
 
 const useStyles = makeStyles({
     root: {
@@ -27,21 +31,43 @@ const useStyles = makeStyles({
     }
 });
 
+
 const GameCard = (props) => {
     const classes = useStyles();
+    const context = useContext(GameContext);
+    const [trailerData, setTrailer] = useState();
+    const [coverURI, setCoverURI] = useState();
+
+    const getBoxArt = () => {
+        fetch(`http://localhost:5000/boxart/${props.game.cover}`)
+            .then(response => response.json())
+            .then(data => setCoverURI(data));
+    }
+    const getGameTrailer = () => {
+        fetch(`http://localhost:5000/video/${props.game.name}`)
+            .then(videoId => setTrailer(videoId))
+            .catch(e => console.log(e))
+    }
+    useEffect(() => {
+        getBoxArt();
+        //getGameTrailer();
+    }, [props.game.name])
 
     return (
         <Box mx={0.5}>
+            {console.log(trailerData)}
             <Card className={classes.root} raised >
-                
                 <CardActionArea>
-                    <CardMedia
-                        className={classes.media}
-                        image={`https://static-cdn.jtvnw.net/ttv-boxart/${props.game.name}-${345}x${345}.jpg`}
-                        title={props.game.name}
-                    >
-                    <AddCircleOutlineIcon className={classes.icon}/>
-                    </CardMedia>
+                    {
+                        coverURI === undefined ?
+                            <img src={"https://static-cdn.jtvnw.net/ttv-static/404_boxart-345x345.jpg"}></img> :
+                            <CardMedia
+                                className={classes.media}
+                                image={`https://images.igdb.com/igdb/image/upload/t_cover_big/${coverURI[0].image_id}.jpg`}
+                                title={props.game.name}>
+                                <AddCircleOutlineIcon className={classes.icon} fontSize="large" />
+                            </CardMedia>
+                    }
                     <CardContent>
                         <Typography gutterBottom variant="h5" component="h2">
                             {props.game.name}
@@ -50,17 +76,34 @@ const GameCard = (props) => {
                             {
                                 props.game.game_modes === undefined ? <></> :
                                     props.game.game_modes.map((modes) => {
-                                        console.log(modes.name)
-                                        return <Chip label={modes.name} />
+                                        return <Box mx={0.2} component="span"><Chip label={modes.name} /></Box>
                                     })
                             }
                         </Typography>
                     </CardContent>
                 </CardActionArea>
                 <CardActions>
-                    <Button size="small" color="primary">
-                        See More
-        </Button>
+                    <GameDetail type={"See More..."}>
+                        <h2 id="spring-modal-title">{props.game.name}</h2>
+                        <p id="spring-modal-description">{props.game.summary}</p>
+                    </GameDetail>
+                    {
+                        trailerData === undefined ? 
+                            <GameDetail type={"Error"}>
+                                <p>Video not available</p>
+                            </GameDetail> :
+                            <GameDetail game={props.game} type={"Watch Trailer"}>
+                                <iframe
+                                    width="853"
+                                    height="480"
+                                    src={`https://www.youtube.com/embed/${trailerData}`}
+                                    frameBorder="0"
+                                    allow="autoplay; clipboard-write; encrypted-media; picture-in-picture"
+                                    allowFullScreen
+                                    title={props.game.name}
+                                />
+                            </GameDetail>
+                    }
                 </CardActions>
             </Card>
         </Box>
