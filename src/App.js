00dde36fd6da309e5b10b556cebef5e4d79df2d0
Jacob T <jacob.tingling@protonmail.com>
2021-05-10 16:39:16 -0400

    Moved search results to other component.

--- src/App.js
+++ src/App.js
@@ -4,33 +4,24 @@ import GameCard from './GameCard';
 import NavBar from './NavBar';
 
 import Paper from '@material-ui/core/Paper'
-import { makeStyles } from '@material-ui/core/styles';
-import { Switch, Route, useLocation } from 'react-router-dom';
-import TabPanel from './TabPanel';
+
+import { Switch, Route  } from 'react-router-dom';
+import List from './List';
+import SearchResult from './SearchResult';
+
 export const GameContext = React.createContext();
 
-const useStyles = makeStyles({
-  root: {
-    backgroundColor: "gray"
-  },
-  card: {
-    maxWidth: "1fr",
-    display: "flex",
-    flexWrap: "wrap",
-    justifyContent: "flex-start"
-  }
-});
+
 
 function App() {
   const [gameNameData, setGameName] = useState();
   const [gameData, setGameData] = useState();
-  const [recommendations, setRecommendations] = useState();
   const [tabValue, setTabValue] = React.useState(0);
 
   const handleTabChange = (event, newValue) => {
       setTabValue(newValue);
   };
-  const classes = useStyles();
+
 
   const submitGame = (e) => {
     e.preventDefault();
@@ -38,12 +29,6 @@ function App() {
       .then(response => response.json())
       .then(data => setGameData(data))
       .catch(e => console.log(e));
-  }
-  const submitRecommendations = () => {
-    fetch(`http://localhost:5000/recommendations`)
-      .then(response => response.json())
-      .then(data => setRecommendations(data))
-      .catch(e => console.log(e))
   }
   const handleGameName = (e) => {
     setGameName(e.target.value)
@@ -57,23 +42,9 @@ function App() {
       })
   }
   const getList = () => {
-    return <TabPanel value={tabValue} index={0}><h1>Tab Panel {tabValue}</h1></TabPanel>
+    //TODO: read data from DB, render <GameCard> with game data
   }
-  /*
-  const handleRecommendedData = () => {
-    return (
-      <ul>{recommendations.Similar.Results.map((games) => {
-        let name = games.Name.replaceAll(' ', "%20")
-        return (
-          <Card>
-            <li key={Math.random()}>{games.Name}</li>
-            <img src={`https://static-cdn.jtvnw.net/ttv-boxart/${name}-300x300.jpg`} alt='box-art' />
-          </Card>
-        )
-      })}</ul>
-    )
-  }
-  */
+
   return (
     <Paper elevation={1}>
       <GameContext.Provider value={{
@@ -84,12 +55,15 @@ function App() {
         gameName: gameNameData
         }}>
         <NavBar/>
-        <div className={classes.card}>
-          {console.log(tabValue)}
-          {
-            gameData !== undefined ? handleGameData() : getList()
-          }
-          
+        <div>
+          <Switch>
+            <Route path="/list/:id">
+              <List></List>
+            </Route>
+            <Route path="/">
+              <SearchResult getGameData={gameData} getGames={handleGameData} />
+            </Route>
+          </Switch>       
         </div>
       </GameContext.Provider>
     </Paper>
---
+++ src/SearchResult.js
@@ -0,0 +1,26 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+    root: {
+      backgroundColor: "gray"
+    },
+    card: {
+      maxWidth: "1fr",
+      display: "flex",
+      flexWrap: "wrap",
+      justifyContent: "flex-start"
+    }
+  });
+
+const SearchResult = (props) => {
+    const classes = useStyles();
+    return (
+        <div className={classes.card}>
+          {
+            props.getGameData !== undefined ? props.getGames() : <></>
+          }
+        </div>
+    )
+}
+
+export default SearchResult;
